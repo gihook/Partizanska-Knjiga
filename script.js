6b126922d3b4923347540a60fc2b0db563f62276
root <root@Kuca.(none)>
2016-02-13 21:04:02 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/*$(document).ready(function() {
+    $('.navigation>ul>li').hover(function(e) 
+    { 
+	setDefaultClasses();
+	$(this).parent().removeClass();
+     	$(this).parent().addClass("active");
+    });
+});
+
+function setDefaultClasses(){
+	$('.navigation>ul>li').each(function(){
+		$(this).removeClass();
+		$(this).addClass("navigation-list");
+	});
+}*/
